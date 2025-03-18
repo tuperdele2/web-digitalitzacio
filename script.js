9c079054fd7a27343ec16176ee76ba14234682d1
tuperdele2 <gerardbh@correu.umanresa.cat>
2025-03-18 19:41:07 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function saludar() {
-    let nom = document.getElementById("nom").value;
-    if (nom) {
-        document.getElementById("missatge").innerText = "Hola, " + nom + "! Gràcies per visitar la meva web.";
-    } else {
-        document.getElementById("missatge").innerText = "Si us plau, escriu el teu nom.";
-    }
-}
