70e5906f006c8a9f26b3e712745e209d1e19c737
tuperdele2 <gerardbh@correu.umanresa.cat>
2025-03-18 19:22:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function saludar() {
+    let nom = document.getElementById("nom").value;
+    if (nom) {
+        document.getElementById("missatge").innerText = "Hola, " + nom + "! Gràcies per visitar la meva web.";
+    } else {
+        document.getElementById("missatge").innerText = "Si us plau, escriu el teu nom.";
+    }
+}
