228c39108844d61a95ef683a2835cc86430cf6f5
tuperdele2 <gerardbh@correu.umanresa.cat>
2025-03-19 08:21:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
 function jugar() {
-    const textos = [
-        "Aquest text ha estat generat per una IA.",
-        "Aquest text ha estat escrit per un humà."
-    ];
-    
-    const aleatori = Math.floor(Math.random() * textos.length);
-    document.getElementById("resultat").innerText = textos[aleatori];
-    
-    // Redirigir a una pàgina web
     window.location.href = "https://www.humanornot.ai/"; // Canvia aquesta URL per la que vulguis
 }
